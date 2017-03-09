fef862a36bf4ef4c07863f6bd5958d2bdfb042a2
nullptru <nullptru@gmail.com>
2017-03-09 09:41:55 +0800

    最low的方式完成每个tab界面搭建，视情况重构

---
+++ src/components/Tab/MedicineManagement.jsx
@@ -0,0 +1,46 @@
+import React, {Component, PropTypes} from 'react'
+import {Col,Row,Table,Button, Label} from 'react-bootstrap/lib'
+
+export default class MedicineManagement extends Component {
+    render() {
+        let tableJson = this.props.tables;
+
+        let tableDoms = [];
+        tableJson.forEach((row)=>{
+            tableDoms.push(<tr key={row.id}>
+                <td>{row.username}</td>
+                <td>{row.accessSections}</td>
+                <td>
+                    <input type="checkbox" id="isActive" defaultChecked={row.isActive}/>
+                </td>
+                <td><Button bsSize='xsmall'>修改</Button>&nbsp;&nbsp;<Button bsSize='xsmall'>删除</Button></td>
+            </tr>);
+        });
+        return <div>
+            <Row>
+                <Col sm={4} md={4}>
+                    <h3>药品管理</h3>
+                </Col>
+                <Col sm={3} md={3} mdOffset={4} smOffset={4}>
+                    <a><h5 style={{marginTop: '40px'}}><Label>+</Label> 添加角色</h5></a>
+                </Col>
+            </Row>
+            <Row>
+                <Table striped bordered condensed hover responsive>
+                    <thead>
+                    <tr>
+                        <th>角色名</th>
+                        <th>可访问科室</th>
+                        <th>是否启用</th>
+                        <th>操作</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tableDoms}
+                    </tbody>
+                </Table>
+            </Row>
+            <Button className="tab-center">保存</Button>
+        </div>
+    }
+}
