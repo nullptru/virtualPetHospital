da0583379fdd6fcdfb22d2356b9ca2d46c62b9f2
nullptru <nullptru@gmail.com>
2017-03-11 18:51:27 +0800

    添加fetch-mock模块进行模拟测试，完成login逻辑

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
 import {Form, FormControl, FormGroup, Button, Row, Col, Image, Grid} from 'react-bootstrap/lib'
 import {browserHistory} from 'react-router';
+import 'whatwg-fetch'
 
 import '../css/login.css'
+
+import '../mock/loginMock' //用于测试
 
 export default class Login extends Component {
     constructor(){
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            username : '',
+            password : '',
+            captcha : '',
+            errorMsg : ''
+        }
     }
     generateCaptcha(){
          // this.captcha1 = ccap().get();
@@ -18,18 +27,44 @@ export default class Login extends Component {
 
     componentDidMount(){
         //this.generateCaptcha();
+        sessionStorage.setItem('isValidate', false);
     }
 
     handleSubmit(e){
         e.preventDefault();
-        if (this.validate()){
-            const path = '/main';
-            browserHistory.push(path);
-        }
+        this.validate()
+            .then((json)=> {
+                if (json.isValidate){
+                    sessionStorage.setItem('isValidate', true);
+                    const path = '/main';
+                    browserHistory.push(path);
+                }else{//验证不通过显示错误
+                    this.setState({errorMsg: json.errorMsg});
+                }
+            })
+            .catch(function (ex) {
+               console.log('validate failed',ex);
+            });
     }
 
     validate(){
-        return true;
+        let isValidatePromise;
+        isValidatePromise = fetch('http://localhost:3001/login/validate',{
+                method : 'post',
+                body : JSON.stringify({
+                    username : this.state.username,
+                    password : this.state.password,
+                    captcha : this.state.captcha
+                })
+            })
+            .then((response)=>{
+                return response.json();
+            }).then(function(json) {
+                return json;
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            });
+        return isValidatePromise;
     }
 
     render() {
@@ -38,15 +73,15 @@ export default class Login extends Component {
                 <Col>
                     <Form>
                         <FormGroup controlId="user" bsSize="large">
-                            <FormControl type="text" placeholder="用户名" defaultValue="" className="input" ref={(username)=>{this.username = username}}/>
+                            <FormControl type="text" placeholder="用户名" className="input" value={this.state.username} onChange={(e)=>{this.setState({username : e.target.value})}}/>
                         </FormGroup>
                         <FormGroup controlId="pwd" bsSize="large">
-                            <FormControl type="password" placeholder="密码" defaultValue="" className="input" ref={(pwd)=>{this.password = pwd}}/>
+                            <FormControl type="password" placeholder="密码" className="input" value={this.state.password} onChange={(e)=>{this.setState({password : e.target.value})}}/>
                         </FormGroup>
                             <FormGroup controlId="verification" bsSize="large">
                                 <Row>
                                     <Col md={7}>
-                                        <FormControl type="text" placeholder="验证码" defaultValue="" className="input" ref={(captcha)=>{this.captcha = captcha}}/>
+                                        <FormControl type="text" placeholder="验证码" className="input" value={this.state.captcha} onChange={(e)=>{this.setState({captcha : e.target.value})}}/>
                                     </Col>
                                     <Col md={5}>
                                         <Image/>
@@ -56,7 +91,7 @@ export default class Login extends Component {
                         <Button type="submit" bsStyle="danger" style={{width:"100%"}} onClick={this.handleSubmit}>登 陆</Button>
                     </Form>
                     <hr/>
-                    <p hidden>Error Message</p>
+                    <p className="validate-error">{this.state.errorMsg}</p>
                 </Col>
             </Row>
         </Grid>
