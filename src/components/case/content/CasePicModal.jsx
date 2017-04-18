c2c6aa842249802caf444fb893500943cb8dbb11
cbd109 <zhouchunyi_gege@163.com>
2017-04-18 11:36:33 +0800

    0418 pic

--- src/components/case/content/CasePicModal.jsx
+++ src/components/case/content/CasePicModal.jsx
@@ -8,23 +8,6 @@ export default function CasePicModal(props) {
     let itemDom = [], count = 0;
     let str = props.imgList;
     console.info('origin str:' + str)
-    // let imgArrStr = str.substring(str.indexOf('[') + 1, str.indexOf(']'));
-    // console.info(imgArrStr)
-    //
-    // let index = 1;
-    // imgArrStr.split(',').forEach((img) => {
-    //     let trimImg = img.trim();
-    //     let img_url = trimImg.substring(1, trimImg.length - 1);
-    //     console.info("img_url:"+img_url);
-    //
-    //     itemDom.push(
-    //         <Carousel.Item key={count++}>
-    //             <Image width={img_width} height={img_height}
-    //                    src={img_url} responsive/>
-    //         </Carousel.Item>
-    //     );
-    //     index++;
-    // });
 
     str.split(' ').forEach((img) => {
         let img_url = img.substring(1, img.length - 1);
